ff073c6f4ec95065f7477b5d3c17d4cf82fd7cc5
Rui Ying <yingrui205@gmail.com>
2020-07-02 23:00:03 +0800

    Update project

--- app/src/main/java/io/robertying/androidcurlexample/MainActivity.java
+++ app/src/main/java/io/robertying/androidcurlexample/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
